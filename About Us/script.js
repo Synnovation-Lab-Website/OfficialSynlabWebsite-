264d3a47033af768bd2c4968e6619d9d1f05cbe6
Sean <148463374+SeanDolan0@users.noreply.github.com>
2024-04-30 23:06:19 -0400

    more stuff

--- About Us/script.js
+++ About Us/script.js
@@ -5,6 +5,7 @@ const text = document.getElementById("text");
 const img = document.getElementById("img");
 const nameTeacher = document.getElementById("name");
 const body = document.getElementById("body");
+const html = document.getElementById("html");
 
 // Teacher info pop-up
 function teacherInfo(teacher) {
@@ -16,14 +17,16 @@ function teacherInfo(teacher) {
   popUp.style.top = scrollPos + "px";
   text.innerHTML = teacherBio[teacher].bio;
   nameTeacher.innerHTML = teacherBio[teacher].name;
-  img.src = teacherBio[teacher].img;
+  // img.src = teacherBio[teacher].img;
   body.style.overflow = "hidden";
+  html.style.overflow = "hidden";
 }
 
 // Close the pop-up
 function infoClear() {
   popUp.classList.remove("custom-class-popUp");
-  body.style.overflow = "scroll";
+  // body.style.overflow = "scroll";
+  html.style.overflowY = "scroll";
   x.style.cursor = "default";
   setTimeout(function () {
     popUp.style.zIndex = "-1";
