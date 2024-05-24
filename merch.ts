9f016099234647700e58502ee48102866dc05aeb
Evan Hoge <evanhoge123@gmail.com>
2024-05-24 10:59:51 -0400

    Reformatted page. Changed layout and added some display shenanigans. Links to AmongUs page lmao.

--- merch.ts
+++ merch.ts
@@ -2,12 +2,6 @@ import { JSDOM } from "jsdom";
 import { writeFileSync } from "fs";
 const DOMParser = new JSDOM().window.DOMParser;
 
-
-// fetch html from rokkitwear
-//  https://www.rokkitwear.com/school/56516-synnovation-lab/
-// parse html and collect most popular items
-// present to user
-
 let dataPromise = fetch("https://www.rokkitwear.com/school/56516-synnovation-lab/");
 dataPromise.then((response) => {
   return response.text();
@@ -15,20 +9,30 @@ dataPromise.then((response) => {
   let domParser = new DOMParser();
   let dom = domParser.parseFromString(html, "text/html");
   dom.querySelectorAll("#productImageDisplay").forEach((x) => {
-    parseProduct(x);
+    parseImage(x);
+  });
+  dom.querySelectorAll(".rw-text10black").forEach((x) => {
+    parseName(x);
   });
   let js_file_output = "let image_sources = [\n";
-  for (let src of imageSources) {
-    js_file_output += "\t\"" + src + "\",\n";
+  for (let i = 0; i < imageSources.length; i++) {
+    js_file_output += "\t[\"" + imageSources[i] + "\", \"" + merchNames[i] + "\"], \n";
   }
   js_file_output += "];";
   writeFileSync("./Merch/sources.js", js_file_output, "utf8");
 });
 
 let imageSources: string[] = [];
-function parseProduct(p: Element) {
-  let image = p.querySelector("img");
+function parseImage(i: Element) {
+  let parent_div = i.parentNode;
+  let image = i.querySelector("img");
   let imageSource = image?.src;
   if (imageSource === undefined) return;
   imageSources.push(imageSource);
+}
+
+let merchNames: string[] = [];
+function parseName(p: Element) {
+  let name = p.innerHTML.split("</span>")[1].replace("\n", "");
+  merchNames.push(name);
 }
